b866340763831bc47372826633fb76a3f28f7053
vanessadmoura <nessa.moura8@gmail.com>
2023-08-29 18:42:54 -0300

    New methods added and docker and database configuration

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -36,6 +36,18 @@ public class AnimeController {
         return new ResponseEntity<>(animeService.save(anime),HttpStatus.CREATED);
     }
 
+    @DeleteMapping (path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Anime> replace(@RequestBody Anime anime){
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
 
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -32,4 +32,14 @@ public class AnimeService {
         animes.add(anime);
         return anime;
     }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
+
+    }
 }
